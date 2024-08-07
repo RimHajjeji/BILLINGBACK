eb6089355350289350bc09cb46cbdb22ac04c633
RimHajjeji <rimhajjeji210@gmail.com>
2024-08-07 10:43:41 +0100

    HH

--- Routes/DevisRoute.js
+++ Routes/DevisRoute.js
@@ -1,17 +1,16 @@
+// Routes - DevisRoute.js
 const express = require('express');
+const DevisController = require('../Controllers/DevisController');
 const router = express.Router();
-const DevisController = require('../controllers/DevisController');
 
-// Routes
 router.post('/add', DevisController.create_devis);
-
-router.get('/', DevisController.show_devis);
-router.get('/:id', DevisController.show_devis_byid);
-router.put('/update/:id', DevisController.update_devis);
-router.put('/update_etat/:id', DevisController.update_devis_etat);
-router.delete('/:id', DevisController.delete_devis);
-router.get('/company/:id', DevisController.Get_devis_Prix_by_company_id);
-router.get('/client/:id', DevisController.Get_devis_byclient);
-router.get('/oneclient/:id', DevisController.Get_ons_devis_byclient);
+router.get('/show', DevisController.show_devis);
+router.get('/show/:id', DevisController.show_devis_byid);
+router.get('/show/price/devis/:id', DevisController.Get_devis_Prix_by_company_id);
+router.get('/show/devis/client/:id', DevisController.Get_devis_byclient);
+router.get('/show/last/devis/company/:id', DevisController.Get_ons_devis_byclient);
+router.put('/upd/:id', DevisController.update_devis);
+router.put('/etats/upd/:id', DevisController.update_devis_etat);
+router.delete('/delete/:id', DevisController.delete_devis);
 
 module.exports = router;
